08571b84f2477131fa981442b39a4b97e09b5842
Nima Afsharfar <ne3mer@gmail.com>
2019-05-13 14:30:03 +0430

    firstcommitallproject

---
+++ Movie Factory/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Movie_Factory.Models;
+
+namespace Movie_Factory.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+           
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "اطلاعات تماس با ما";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        
+
+    }
+}
---
+++ Movie Factory/Models/student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieFactory.Models
+{
+    public class student
+{
+    public int StudentID { get; set; }
+
+
+    public string Name { get; set; }
+
+
+    public string Address { get; set; }
+
+
+    public string Mobile { get; set; }
+
+
+}
+}
