7870d15a1a5032e4243d430f26a027705983452d
Nima Afsharfar <ne3mer@gmail.com>
2019-05-28 14:27:18 +0430

    add movie controller details and index table & fix partial view & movie controller added

---
+++ Movie Factory/Controllers/MoviesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Movie_Factory.Data;
+
+
+namespace Movie_Factory.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly ApplicationDbContext _Context;
+        public MoviesController (ApplicationDbContext Context)
+        {
+            _Context = Context;
+        }
+        public IActionResult Index()
+        {
+            var list = _Context.Movies.ToList();
+            return View(list);
+        }
+        public IActionResult Details(int? id)
+        {
+            if (id==null)
+            {
+                return NotFound() ;
+            }
+            var movie = _Context.Movies.FirstOrDefault(m => m.Id == id);
+            if (movie==null)
+            {
+                return NotFound();
+            }
+            return View(movie);
+        }
+    }
+}
